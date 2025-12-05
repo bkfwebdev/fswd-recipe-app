ab944f027e66fbf3157c823197da06482e926db5
Bryant K. Feld <bryant.feld@gmail.com>
2025-12-05 23:42:45 +0000

    more socket updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import http from 'http'
 import { app } from './app.js'
-import { connectToDatabase } from './db/init.js'
+import { initDatabase } from './db/init.js'
 import { initializeSocket } from './socket.js'
 
 const PORT = process.env.PORT || 3000
@@ -11,12 +13,11 @@ const httpServer = http.createServer(app)
 initializeSocket(httpServer)
 
 try {
-  await connectToDatabase(process.env.DATABASE_URL)
-  console.log('successfully connected to database:', process.env.DATABASE_URL)
+  await initDatabase()
+  httpServer.listen(PORT, () => {
+    console.log(`express server running on http://localhost:${PORT}`)
+  })
 } catch (err) {
   console.error('error connecting to database:', err)
 }
 
-httpServer.listen(PORT, () => {
-  console.log(`express server running on http://localhost:${PORT}`)
-})
